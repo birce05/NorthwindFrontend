13db5cb6197293982105fd832eb5e338eef918b9
birce05 <birsenberrin@gmail.com>
2021-03-23 00:16:29 +0300

    20,3 yenileme

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product';
 
 @Component({
   selector: 'app-product',
@@ -6,19 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
-
-  product1: any = { productId: 1, productName: 'bardak', categoryId: 1, unitPrice: 5 }
-  product2: any = { productId: 2, productName: 'laptop', categoryId: 1, unitPrice: 5 }
-  product3: any = { productId: 3, productName: 'mouse', categoryId: 1, unitPrice: 5 }
-  product4: any = { productId: 4, productName: 'keyboard', categoryId: 1, unitPrice: 5 }
-  product5: any = { productId: 5, productName: 'camera', categoryId: 1, unitPrice: 5 }
-
-  products = [
-    this.product1,
-    this.product2,
-    this.product3,
-    this.product4,
-    this.product5]
+ 
+  products:Product []= [];
+  
   constructor() { }
 
   ngOnInit(): void {
---
+++ src/app/models/category.ts
@@ -0,0 +1,10 @@
+export interface Category{
+    categoryId:number;
+    categoryName:string;
+    
+}
+
+// PRODUCT.COMPONENT.TS DOSYAN YOK, 
+// PRODUCT COMPONENTINI TEKRAR OLUSTURACAĞIM
+// spec dosyalarına gerek yok onlar test için, silebiliriz
+// şimdi kodlarını taşıyalım eski product dan
