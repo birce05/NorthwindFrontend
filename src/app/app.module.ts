1b00d665d882a5219b2349e0cfd6983d97816adb
birce05 <birsenberrin@gmail.com>
2021-03-26 14:03:51 +0300

    26,mar commit1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-
+import {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './components/product/product.component';
 import { CategoryComponent } from './components/category/category.component';
@@ -17,7 +16,8 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,18 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
+import { ProductService } from 'src/app/services/product.service';
 
+//axios,fetch kütüphane
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  styleUrls: ['./product.component.css'],
 })
 export class ProductComponent implements OnInit {
- 
-  products:Product []= [];
+  products: Product[] = [];
+  dataLoaded=false;
+  getCategories: any;
   
-  constructor() { }
+    
+  
+  constructor(private productService:ProductService) {}
 
   ngOnInit(): void {
+    this.getCategories();
   }
-
+  getProducts() {
+    this.productService.getProducts().subscribe(response=>{
+      this.products=response.data
+      this.dataLoaded=true;
+        
+    })
+  }
 }
